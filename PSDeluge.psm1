4686c0be2296973733eafff10b88db6227345fe6
briansworth <b.farnsworth47@gmail.com>
2016-12-28 00:01:35 -0500

    Initial commit: Created GetFiles function that works, the main function Get-DelugeTasks is not working, just a start.

---
+++ PSDeluge.psm1
@@ -0,0 +1,84 @@
+﻿function GetFiles{
+    [CmdletBinding()]
+    Param(
+        [Parameter(
+            ValueFromPipeline=$true,
+            Mandatory=$true            
+        )]
+        [Collections.ArrayList]$obj
+    )
+    Begin{
+        [String]$pctRegex='(?<=Progress: )[0-9]{1,3}\.[0-9]{2}(?=\%)'
+        [String]$sizeRegex='(?<=\()[[0-9]{1,4}\.[0-9]{1,2}\s(MiB|KiB|GiB|TiB)(?=\))'
+        [String]$priorityRegex='(?<=Priority:)\s(Normal|Low|High)'
+    }
+    Process{
+        [int]$findex=$obj.IndexOf('  ::Files')
+        [int]$pindex=$obj.IndexOf('  ::Peers')
+        if(($pindex - $findex) -eq 1){
+            return $null
+        }
+        [int]$fcount=($pindex - $findex - 1)
+        [Collections.ArrayList]$fileList=@()
+        for ($i=$findex + 1; $i -le $findex + $fcount; $i++){
+            [String]$line=$obj[$i]
+            [float]$pct=[Regex]::Match($line,$pctRegex).Value
+            [PSObject]$sizeResult=[Regex]::Match($line,$sizeRegex)
+            [string]$sizeStr=$sizeResult.Value
+            [double]$size=Invoke-Expression `
+              -Command $sizeStr.Replace('i','').Replace(' ','')
+            [String]$priority=[Regex]::Match($line,$priorityRegex).Value
+            $build=New-Object -TypeName Text.StringBuilder
+            for($j=0;$j -lt ($sizeResult.Index -1);$j++){
+                [void]$build.Append($line[$j])
+            }
+            [String]$name=$build.ToString().Trim()
+            $file=New-Object -TypeName psobject -Property @{
+                'Name'=$name;
+                'PercentComplete'=$pct;
+                'Size'=$size;
+                'Priority'=$priority;
+            }
+            [void]$fileList.Add($file)
+        }
+        return $fileList
+    }
+    End{}
+}
+
+function Get-DelugeTasks {
+    [CmdletBinding(DefaultParameterSetName='Name')]
+    Param(
+        [Parameter(
+            Position=0,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName='Name'
+        )]
+        [String]$name,
+
+        [Parameter(
+            Position=0,
+            Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true,
+            ParameterSetName='Id'
+        )]
+        [String]$id
+    )
+    Begin{
+
+    }
+    Process{
+        Try{
+            [Collections.ArrayList]$list=deluge info $name -v
+            foreach($line in $list){
+                
+            }
+        }Catch{
+            $e=$_
+            Write-Error $e
+        }
+    }
+    End{}
+}
+
+Get-DelugeTasks -name 'Sully  2016 720p BrRip x264 - 2HD'
